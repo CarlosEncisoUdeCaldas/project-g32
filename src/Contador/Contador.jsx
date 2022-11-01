ce6c85939f04a25427b5f176a629613a809b7027
CarlosEncisoUdeCaldas <carlosenciso.tic@ucaldas.edu.co>
2022-10-31 19:47:10 -0500

    Contador ok

--- src/Contador/Contador.jsx
+++ src/Contador/Contador.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types"
 import { useState } from "react";
+import Button from "./Button";
 
 
 const Contador = ( { value } ) => {
     //definicion del useState count
     const [ count , setCount ] = useState( value ) ;
     
-    //funciones tipo handle
+    //funciones tipo handle de contador
     function handleAddClick (e) {
         // console.log ( value ) //incrementado el valor de value en 1
         // // console.log(e.target.type)
@@ -16,11 +17,29 @@ const Contador = ( { value } ) => {
         console.log( count );
     }
 
+    //funcion para restar
+    const handleResta = () => {
+        setCount( count - 1 )
+    }
+
+    //funcion reset
+    const handleReset = () => {
+        setCount( value ) ;
+    }
+
     return(
         <>
             <h1>Contador</h1>
-            <p id='demo'> { count }</p>
-            <button type="button" name="add" onClick = { handleAddClick }> + 1 </button>
+            <p> { count } </p>
+            <Button title="+ 1" handleFunction= { handleAddClick } />
+            <Button title="- 1" handleFunction= { handleResta } />
+            <Button title="Reset" handleFunction = { handleReset } />
+
+            {/* <button type="button" name="add" onClick = { handleAddClick }>+ 1</button> */}
+            {/* <button type="button" name="resta" onClick = { handleResta }>- 1</button> */}
+            {/* <button type="button" name="reset" onClick = { handleReset }>Reset</button> */}
+            {/* { count !== 0 && <Button count={count} handleReset = { handleReset } /> } */}
+            
         </>
     )
 }
